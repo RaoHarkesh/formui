754e2f862223b4c9139bf744510a362f0307e213
RaoHarkesh <raoharkesh1998@gmail.com>
2022-08-13 20:14:32 +0530

    gender added and icon added

--- src/components/newcandidate.jsx
+++ src/components/newcandidate.jsx
@@ -8,12 +8,16 @@ const Candidate =()=>{
           <div className="inside insidebutton">
           <button onClick={()=>setEdit(false)}>Create user</button>
           <button onClick={()=>setEdit(true)}>Edit user</button>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
           </div>
           
     </div>
     <div className="create">
         <div className="inside insidecreate">
-            <span>Add new candidate</span>
+            {edit? <span>Edit candidate</span>:<span>Add new candidate</span>}
         </div>
     </div>
     <form action="#">
@@ -46,11 +50,35 @@ const Candidate =()=>{
 
         </div>
 
+
+        <div className="gender-nationality">
+            <div className="inside insidegender">
+                <div className="nationality">
+
+                    <select name="nationality" id="nationality">
+                    <option value="" disabled selected>NATIONALITY</option>
+                        <option value="indian">INDIAN</option>
+                        <option value="Canadian">CANADIAN</option>
+                        <option value="american">AMERICAN</option>
+                    </select>
+                </div>
+                <div className="gender">
+                   <select name="gender" id="gender">
+                    <option value="" disabled selected>GENDER</option>
+                    <option value="male">MALE</option>
+                    <option value="female">FEMALE</option>
+                    <option value="others">OTHERS</option>
+                   </select>
+                    
+                </div>
+            </div>
+      </div>
+
         <div className="email-phone">
             <div className="inside insideemail-phone">
                 <div className="email">
 
-                    <input type="text" id="email" placeholder="EMAIL"/>
+                    <input type="text" id="email" placeholder="E-MAIL"/>
                 </div>
                 <div className="phone">
                     <select name="countries">
